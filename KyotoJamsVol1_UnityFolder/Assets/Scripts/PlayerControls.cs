cc29b9e3f2cf8ad53e9ca768a647e292a50c6e59
Sagar Patel <17.sagar.patel@gmail.com>
2015-04-03 18:53:02 +0900

    Player firing works

---
+++ KyotoJamsVol1_UnityFolder/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour 
+{
+	float fireCooldown = 1.0f;
+	float fireColldownTimeCounter = 0;
+	bool isReadyToFire = true;
+	PlayerWeapon playerWeapon;
+
+	void Start()
+	{
+		playerWeapon = FindObjectOfType<PlayerWeapon>();
+	}
+
+	void Update()
+	{
+		if(isReadyToFire == true)
+		{
+			/*
+			if(Input.touchCount == 1)
+			{    
+				if(Input.GetTouch(0).phase == TouchPhase.Began)
+				{
+
+					Debug.Log("FIRE");
+					// FIRE!
+					isReadyToFire = false;
+
+
+				}
+			}
+			*/
+			if(Input.GetMouseButtonDown(0))
+			{
+				Debug.Log("FIRE");
+				// FIRE!
+				isReadyToFire = false;
+				playerWeapon.FireWeapon();
+			}
+
+		}
+		else
+		{
+			fireColldownTimeCounter += Time.deltaTime;
+
+			if(fireColldownTimeCounter > fireCooldown )
+			{
+				isReadyToFire = true;
+				fireColldownTimeCounter = 0;
+			}
+
+		}
+	}
+
+}
---
+++ KyotoJamsVol1_UnityFolder/Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeapon : MonoBehaviour 
+{
+	public GameObject bulletPrefab;
+	float bulletSpeed = 1.0f;
+
+
+	void Start()
+	{
+
+	}
+
+	public void FireWeapon()
+	{
+		GameObject firedBullet = (GameObject)Instantiate(bulletPrefab);
+		firedBullet.transform.position = transform.position;
+		firedBullet.transform.rotation = transform.rotation;
+		firedBullet.GetComponent<ObjectMove>().moveSpeed = bulletSpeed;
+	}
+
+
+}
