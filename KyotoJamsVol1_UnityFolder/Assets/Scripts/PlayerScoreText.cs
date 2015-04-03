721e5c16969d53b986ea162fa1315b450f541550
Sagar Patel <17.sagar.patel@gmail.com>
2015-04-03 19:09:14 +0900

    Implemented player score text

---
+++ KyotoJamsVol1_UnityFolder/Assets/Scripts/PlayerScoreText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerScoreText : MonoBehaviour 
+{
+	string baseText;
+	Text scoreText;
+	PlayerData playerData;
+
+
+	void Start()
+	{
+		scoreText = GetComponent<Text>();
+		baseText = scoreText.text;
+		playerData = FindObjectOfType<PlayerData>();
+	}
+
+	void Update()
+	{
+		scoreText.text = baseText + playerData.GetPlayerScore().ToString();
+
+	}
+
+
+
+}
